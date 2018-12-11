cdbbbe1b1f89cda3664ab1670692d3f49d28c34f
Loundness <ludo.vic@me.com>
2018-12-11 15:00:52 +0100

    homescreen

--- Components/Screens/HomeScreen.js
+++ Components/Screens/HomeScreen.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Divider } from 'react-native-elements'
+import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import {Divider, Button, } from 'react-native-elements'
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
+    <ImageBackground style={{flex:1}} source={require("../../assets/Images/rockhome.jpg")}>
       <View>
-      <Text onPress= { ()=> this.props.navigation.navigate('Map')}>Hello</Text>
-      <Text onPress= { ()=> this.props.navigation.navigate('Filter')}>Go to filter sa meure</Text>
-      </View>
+       <Image  style={{width: 400, height: 400, marginTop:5}} source={require('../../assets/Icons/musicall.png')}/>
+    	 	<Button
+    	 	 buttonStyle={{borderRadius:25, width:300, height:50, justifyContent: 'center'}}
+             title="Sign in"
+             style={{width:300}}
+             backgroundColor='#dadada'
+             color='#CD3C30'
+             onPress={ ()=> this.props.navigation.navigate('SignIn')}
+             >
+           </Button>
+         
+           <Button
+             buttonStyle={{borderRadius:25, width:300, height:50, justifyContent: 'center', marginTop:10}}
+             title="Sign up"
+             style={{flex:1}}
+             backgroundColor='#dadada'
+             color='#CD3C30'
+             onPress={ ()=> this.props.navigation.navigate('SignUp')}
+             >
+           </Button>
+
+       </View>
+      </ImageBackground> 
     );
   }
-}
+};
+;
+
+
+
+
